627f25fc147fb52f550f61be61f1567b4ea00d52
Estela25 <USIS035821@ugb.edu.sv>
2022-02-23 22:33:54 -0500

    diseño de activitys

---
+++ app/src/main/java/com/example/parcial_grupo4/Recibir_datos.java
@@ -0,0 +1,14 @@
+package com.example.parcial_grupo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recibir_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibir_datos);
+    }
+}
